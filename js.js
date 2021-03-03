321d668a4ce49a45e45bc85265347f34c55a5c6b
Marco Vila <marco.vila@hotmail.com>
2021-03-03 12:53:32 -0500

    commit 03/03

--- js.js
+++ js.js
@@ -1,39 +1,61 @@
 $(function(){
+    var $container=$('#app-body').find('.tv-shows');
+    var template='<article class="tv-show">'+
+    '<div class="left img-container">'+
+        '<img src=":img:" alt=":img alt:">  '+   
+    '</div>'+
+    '<div class="right info">'+
+        '<h1>:name:</h1>'+
+        '<p>:summary:</p>'+
+    '</div>'+
+'</article>';  
+
+    function renderShows(data){
+        data.forEach(function (show){
+            var article=template
+            .replace(':name:',show.name)
+            .replace(':img:',show.image.medium)
+            .replace(':summary:',show.summary)
+            .replace(':img alt:',show.name+" Logo")
+
+            var $article=$(article);
+            $container.append($article);
+        })    
+    }
+
+
     //submit search form
     $('#app-body').find('form').submit(function (ev){
-        //08.45
-        //video c
+
         ev.preventDefault();
         var busqueda=$(this).find('input[type="text"]').val();
-        console.log('se ha buscado: '+busqueda);
-    })  
+        //console.log('se ha buscado: '+busqueda);
 
-    var template='<article class="tv-show">'+
-                        '<div class="left img-container">'+
-                            '<img src=":img:" alt=":img alt:">  '+   
-                        '</div>'+
-                        '<div class="right info">'+
-                            '<h1>:name:</h1>'+
-                            '<p>:summary:</p>'+
-                        '</div>'+
-                    '</article>';                
+        $container.find('.tv-show').remove();
+        var $loader=$('<div class="loader">');
+        $loader.appendTo($container);
+        $.ajax({
+            url:'http://api.tvmaze.com/search/shows',
+            data:{q:busqueda},
+            success:function(res,textStatus,xhr){
+
+                $loader.remove();                                
+
+                var shows= res.map(function(e){
+                    return e.show;
+                })
+                renderShows(shows); 
+            }
+        });
+    })  
+              
     //request de shows
     $.ajax({
         url:'http://api.tvmaze.com/shows',
         success:function(data,textStatus,xhr){
-            //console.log(data);
-            var $container=$('#app-body').find('.tv-shows');
-            $container.find('.loader').remove()
-            data.forEach(function (show){
-                var article=template
-                .replace(':name:',show.name)
-                .replace(':img:',show.image.medium)
-                .replace(':summary:',show.summary)
-                .replace(':img alt:',show.name+" Logo")
-
-                var $article=$(article);
-                $container.append($article);
-            })
+            //console.log(data);            
+            $container.find('.loader').remove();
+            renderShows(data);
         }
     });          
 })
